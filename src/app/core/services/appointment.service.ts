f9448e5bc2da1d0a482f8ed89100d08accbe10f5
andreatpr <andreatapiapr@gmail.com>
2024-11-03 22:53:01 -0500

    appointments section

--- src/app/core/services/appointment.service.ts
+++ src/app/core/services/appointment.service.ts
@@ -2,6 +2,8 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Availability } from '../../shared/models/availability.model';
+import { Appointment } from '../../shared/models/appointment.model';
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +21,11 @@ export class AppointmentService {
     return id ? Number(id) : null;
   }
   
+  getAvailabilitySchedule(availabilityId: number): Observable<Availability> {
+    return this.http.get<Availability>(`${environment.baseURL}/availability/${availabilityId}`);
+  }
 
+  addAppointment(appointment: Appointment, userId: number): Observable<Appointment>{
+    return this.http.post<Appointment>(`${this.baseURL}/${userId}`, appointment);
+  }
 }
